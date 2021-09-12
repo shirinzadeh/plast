aedd5cb52d7080b70e5ad0241a67b0b8564af522
Tahmasib Shirinzade <tshzade@gmail.com>
2021-09-12 23:34:26 +0400

    changed hero section image

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     * TEAM OWL CAROUSEL
     */
    $(".owl-carousel").owlCarousel({
-      autoplay: true,
+      autoplay: false,
       autoplayTimeout:2500,
       autoplaySpeed: 1000,
       autoplayHoverPause: true,
